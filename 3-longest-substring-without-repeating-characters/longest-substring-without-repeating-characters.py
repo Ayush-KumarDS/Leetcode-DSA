e1fbdaee6843e4b20f14d7f74b754d957e45852f
Ayush-KumarDS <ayushkeshav2025@gmail.com>
2026-09-17 14:20:53 +0530

    Time: 418 ms (42.99%) | Memory: 16.9 MB (10.31%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
@@ -16,6 +16,3 @@ class Solution(object):
             max_len = max(max_len, right - left + 1)
     
         return max_len
-
-# Example test
-print(Solution().lengthOfLongestSubstring("abcabcbb"))  # Output: 3
